6676acc937f5d4360350135d3484bf9c55292a3f
mertsoyer <75367371+mertsoyer@users.noreply.github.com>
2021-10-26 22:46:35 +0300

    Add project files.

---
+++ BlogApplication.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogApplication.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
